e2f1e5123b14d6a907a6d46910fc67c571771d97
davdwhyte87 <kingstonwhyte87@gmail.com>
2018-12-19 18:00:42 +0100

    Add babel compilation form es6 to es5

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+// set input types
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//set up morgan
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
+// set base endpoint
+const generalPrefix = '/api/v1';
+app.get(generalPrefix, (req, res) => {
+    res.status(200).send({ status: 200, message: 'Welcome to clan api' });
+});
+
+app.use('*', (req, res) => res.status(404).send({
+    status: 404,
+    error: 'This route does not exist. You may navigate to the home route at api/v1',
+  }));
+
+  export default app;
---
+++ dist/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express2.default)();
+
+// set input types
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use(_bodyParser2.default.json());
+
+//set up morgan
+app.use((0, _morgan2.default)('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+});
+
+// set base endpoint
+const generalPrefix = 'api/v1';
+app.use(generalPrefix + '/', (req, res) => {
+    res.status(200).json({ status: 200, message: 'Welcome to clan api' });
+});
+
+app.use('*', (req, res) => res.status(404).send({
+    status: 404,
+    error: 'This route does not exist. You may navigate to the home route at api/v1'
+}));
+
+exports.default = app;
+//# sourceMappingURL=app.js.map
