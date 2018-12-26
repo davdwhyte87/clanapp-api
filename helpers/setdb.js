125af7f5e6d489f98baeb79019812872700be65d
David whyte <kingstonwhyte87@gmail.com>
2018-12-26 02:39:11 -0800

    Create setdb.js

---
+++ helpers/setdb.js
@@ -0,0 +1,6 @@
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const sequelize = new Sequelize(process.env.DB_URL, { dialect: 'postgres', logging: false });
+sequelize.sync();
